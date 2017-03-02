70475d5069e9e4949e5cdc830d261f45fd6140d1
SpookyMac <spooky@iMac-de-SpookyMac.local>
2017-03-02 11:15:31 +0100

    Some changes on swift indentation

--- custom.el
+++ custom.el
@@ -6,7 +6,10 @@
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+    ("ce557950466bf42096853c6dac6875b9ae9c782b8665f62478980cc5e3b6028d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (darkburn-theme zerodark-theme avy-zap pivotal-tracker google-this circe json-reformat restclient move-text org-bullets web-mode emmet-mode js2-mode avy smex counsel elpy omnisharp csharp-mode yasnippet dsvn magit solarized-theme exec-path-from-shell use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
