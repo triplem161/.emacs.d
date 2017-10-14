94027b8a686bb877fa82576634639ccadd82c2d1
Triplem <maigmarion@gmail.com>
2017-10-14 13:16:30 +0200

    Removed whitespace mode due to issue with company mode

--- init.el
+++ init.el
@@ -12,3 +12,17 @@
 
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (web-mode undo-tree shader-mode restclient rainbow-mode elpy plantuml-mode pivotal-tracker move-text magit less-css-mode json-reformat avy-zap smex counsel ivy google-this emmet-mode dsvn company omnisharp csharp-mode avy zenburn-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
