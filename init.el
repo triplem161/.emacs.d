a6ec200473393e896b251f0500d5d79f46a5baf9
unknown <maigmarion@gmail.com>
2020-10-26 11:33:17 +0100

    Simplifying my emacs

--- init.el
+++ init.el
@@ -11,18 +11,3 @@
   (require 'use-package))
 
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (expand-region atomic-chrome yasnippet-snippets json-mode web-mode undo-tree shader-mode restclient rainbow-mode elpy plantuml-mode pivotal-tracker move-text magit less-css-mode json-reformat avy-zap smex counsel ivy google-this emmet-mode dsvn company omnisharp csharp-mode avy zenburn-theme use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
